eb284b44d8f3476c61e0369acdb7b07425246421
Roy Moe <hua.zheng@embeddedboys.com>
2026-04-27 08:55:31 +0000

    v0.0.1: scheduler tested and created 3 tasks; 2 print strings, 1 blinks LED.
    Signed-off-by: Roy Moe <hua.zheng@embeddedboys.com>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# SOocip
+# soocip
 
+soocip. aka picoos since the project is already exists.
+
+This is a very small OS that runs on Raspberry Pi Pico (RP2040 | ARM Cortex-M0+)
+
+...
+
+## Links
+
+- [Exception entry and return](https://developer.arm.com/documentation/dui0662/b/The-Cortex-M0--Processor/Exception-model/Exception-entry-and-return)
+
+- [https://github.com/FreeRTOS/FreeRTOS-Kernel/blob/d1f551e2539ef7324013aebf22cd430a690e0d42/portable/GCC/ARM_CM0/portasm.c#L307](https://github.com/FreeRTOS/FreeRTOS-Kernel/blob/d1f551e2539ef7324013aebf22cd430a690e0d42/portable/GCC/ARM_CM0/portasm.c#L307)
--- main.c
+++ main.c
@@ -6,40 +6,40 @@
 #include "core_cm0plus.h"
 #include "systick.h"
 
-#define DEBUG 1
-
 #define pr_debug(...) printf(__VA_ARGS__)
-// #define __always_inline inline __attribute__((always_inline))
+#define __inline      inline __attribute__((always_inline))
 
-#define MAX_TASKS  4
-#define STACK_SIZE 16
+#define MAX_TASKS  3
+#define STACK_SIZE 256
 struct task_struct {
 	u32 *sp;
 };
 
 struct task_struct tasks[MAX_TASKS];
 static uint current_task = 0;
+struct task_struct *current = NULL;
+struct task_struct *next = NULL;
 
 #define __WFI() asm volatile("wfi" ::: "memory")
 
-__always_inline void __ISB(void)
+static __inline void __ISB(void)
 {
 	asm volatile("isb 0xF" ::: "memory");
 }
 
-static __always_inline uint32_t __get_PSP(void)
+static __inline uint32_t __get_PSP(void)
 {
 	uint32_t result;
 	asm volatile("mrs %0, psp" : "=r"(result));
 	return (result);
 }
 
-static __always_inline void __set_PSP(uint32_t p)
+static __inline void __set_PSP(uint32_t p)
 {
 	asm volatile("msr psp, %0" ::"r"(p) :);
 }
 
-static __always_inline uint32_t __get_CONTROL(void)
+static __inline uint32_t __get_CONTROL(void)
 {
 	uint32_t result;
 
@@ -47,144 +47,135 @@ static __always_inline uint32_t __get_CONTROL(void)
 	return (result);
 }
 
-static __always_inline void __set_CONTROL(uint32_t control)
+static __inline void __set_CONTROL(uint32_t control)
 {
 	asm volatile("MSR control, %0" : : "r"(control) : "memory");
 	__ISB();
 }
 
-static void dump_regs(u32 *sp)
+void dump_regs(u32 *sp)
 {
 	u32 *p = sp;
 	unsigned i;
 
-	// pr_debug("============ HARD FAULT ============\n");
-	// pr_debug("R0  = 0x%08X    R8  = 0x%08X\n", (unsigned)push[0],
-	// 	 (unsigned)regs[0]);
-	// pr_debug("R1  = 0x%08X    R9  = 0x%08X\n", (unsigned)push[1],
-	// 	 (unsigned)regs[1]);
-	// pr_debug("R2  = 0x%08X    R10 = 0x%08X\n", (unsigned)push[2],
-	// 	 (unsigned)regs[2]);
-	// pr_debug("R3  = 0x%08X    R11 = 0x%08X\n", (unsigned)push[3],
-	// 	 (unsigned)regs[3]);
-	// pr_debug("R4  = 0x%08X    R12 = 0x%08X\n", (unsigned)regs[4],
-	// 	 (unsigned)push[4]);
-	// pr_debug("R5  = 0x%08X    SP  = 0x%08X\n", (unsigned)regs[5],
-	// 	 (unsigned)sp);
-	// pr_debug("R6  = 0x%08X    LR  = 0x%08X\n", (unsigned)regs[6],
-	// 	 (unsigned)push[5]);
-	// pr_debug("R7  = 0x%08X    PC  = 0x%08X\n", (unsigned)regs[7],
-	// 	 (unsigned)push[6]);
-	// pr_debug("RA  = 0x%08X    SR  = 0x%08X\n", (unsigned)ret_lr,
-	// 	 (unsigned)push[7]);
-	// pr_debug("WORDS @ SP: \n");
-
-	// for (i = 0; i < 8; i++)
-	// 	pr_debug("[sp, #0x%03X = 0x%08X] = 0x%08x\n", i * 4,
-	// 		 (unsigned)&sp[i], (unsigned)sp[i]);
-
-	p += 16;
 	for (i = 0; i < 16; i++)
 		pr_debug("[sp, #0x%03X = 0x%p] = 0x%08X\n", i * 4, p, *(--p));
 
 	pr_debug("\n\n");
 }
 
-void hard_fault_handler_c(uint32_t *push, uint32_t *regs, uint32_t ret_lr)
+void pendsv_handler_c(uint32_t *push, uint32_t *regs, uint32_t ret_lr)
 {
 	uint32_t *sp = push;
 	unsigned i;
 
-	pr_debug("============ PendSV saved regs Dump ============\n");
-	// pr_debug("R0  = 0x%08X    R8  = 0x%08X\n", (unsigned)push[0],
-	// 	 (unsigned)regs[0]);
-	// pr_debug("R1  = 0x%08X    R9  = 0x%08X\n", (unsigned)push[1],
-	// 	 (unsigned)regs[1]);
-	// pr_debug("R2  = 0x%08X    R10 = 0x%08X\n", (unsigned)push[2],
-	// 	 (unsigned)regs[2]);
-	// pr_debug("R3  = 0x%08X    R11 = 0x%08X\n", (unsigned)push[3],
-	// 	 (unsigned)regs[3]);
-	// pr_debug("R4  = 0x%08X    R12 = 0x%08X\n", (unsigned)regs[4],
-	// 	 (unsigned)push[4]);
-	// pr_debug("R5  = 0x%08X    SP  = 0x%08X\n", (unsigned)regs[5],
-	// 	 (unsigned)sp);
-	// pr_debug("R6  = 0x%08X    LR  = 0x%08X\n", (unsigned)regs[6],
-	// 	 (unsigned)push[5]);
-	// pr_debug("R7  = 0x%08X    PC  = 0x%08X\n", (unsigned)regs[7],
-	// 	 (unsigned)push[6]);
-	// pr_debug("RA  = 0x%08X    SR  = 0x%08X\n", (unsigned)ret_lr,
-	// 	 (unsigned)push[7]);
+	printf("%s, %p, PSP : 0x%08X\n", __func__, push, __get_PSP());
 
-	// printf("regs(sp): %08x\n", regs[6]);
-	printf("LR: %08x\n", ret_lr);
-	// pr_debug("WORDS @ SP: \n");
-
-	for (i = 0; i < 16; i++)
-		pr_debug("[sp, #0x%03X = 0x%08X] = 0x%08x\n", i * 4,
-			 (unsigned)sp, *(sp--));
+	pr_debug("============ PendSV regs Dump ============\n");
 
 	// for (i = 0; i < 16; i++)
-	// 	pr_debug("[sp, #0x%03X = 0x%p] = 0x%08x\n", i * 4, sp, *(--sp));
+	// 	pr_debug("[psp, #0x%03X = 0x%08X] = 0x%08X\n", i * 4,
+	// 		 (unsigned)sp, *(--sp));
+
+	printf("r11 = %08X\n", sp[0]);
+	printf("r10 = %08X\n", sp[1]);
+	printf("r9  = %08X\n", sp[2]);
+	printf("r8  = %08X\n", sp[3]);
+	printf("r7  = %08X\n", sp[4]);
+	printf("r6  = %08X\n", sp[5]);
+	printf("r5  = %08X\n", sp[6]);
+	printf("r4  = %08X\n", sp[7]);
+
+	printf("--------------------------------------------------\n");
+
+	printf("r0  = %08X\n", sp[8]);
+	printf("r1  = %08X\n", sp[9]);
+	printf("r2  = %08X\n", sp[10]);
+	printf("r3  = %08X\n", sp[11]);
+	printf("r12 = %08X\n", sp[12]);
+	printf("lr  = %08X\n", sp[13]);
+	printf("pc  = %08X\n", sp[14]);
+	printf("xpsr= %08X\n", sp[15]);
+
+	printf("--------------------------------------------------\n");
+
+	// for (i = 0; i < 16; i++)
+	// 	pr_debug("[regs, #0x%03X = 0x%08X] = 0x%08X\n", i,
+	// 		 (unsigned)regs, *(--regs));
+
+	// printf("--------------------------------------------------\n");
+
+	// printf("lr: 0x%08X\n", ret_lr);
+
+	// printf("--------------------------------------------------\n");
 
 	pr_debug("\n\n");
+
+	for (;;)
+		;
 }
 
-void task1_func()
+void dump_r0_r1_r2(uint32_t *r0, uint32_t *r1, uint32_t *r2)
 {
+	printf("R0: 0x%08X = 0x%08X\n", r0, *r0);
+	printf("R1: 0x%08X = 0x%08X\n", r1, *r1);
+	printf("R2: 0x%08X = 0x%08X\n", r2, *r2);
+}
+
+void task1_func(void *data)
+{
+	static uint count = 0;
 	for (;;) {
-		printf("%s\n", __func__);
-		for (volatile uint i = 0; i < 100000; i++)
+		printf("\n++++++++++ %s, %04d ++++++++++\n", (char *)data,
+		       count++);
+		for (volatile uint i = 0; i < 1000000; i++)
 			;
 	}
 }
 
-void task2_func()
+void task2_func(void *data)
 {
+	static uint count = 0;
 	for (;;) {
-		printf("%s\n", __func__);
-		for (volatile uint i = 0; i < 100000; i++)
+		printf("\n++++++++++ %s, %04d ++++++++++\n", (char *)data,
+		       count++);
+		for (volatile uint i = 0; i < 1000000; i++)
 			;
 	}
 }
 
-void isr_pendsv(void)
+void task3_func(void *data)
 {
-	// pr_debug("%s, %d\n", __func__, 123);
-	// dump_regs(tasks[current_task].sp);
+	int led_pin = PICO_DEFAULT_LED_PIN;
 
-	asm volatile(
-		".syntax unified\n\t"
-		"mrs r0, psp\n\t"
-		"push     {r4, lr}\n\t"
-		"mov r12, r0\n\t"
-		"mov r0, r8\n\t"
-		"mov r1, r9\n\t"
-		"mov r2, r10\n\t"
-		"mov r3, r11\n\t"
-		"push {r0-r7}\n\t"
-		"	mov		r0, r12									\n\t"
-		"	mov		r1, sp									\n\t"
-		"	mov		r2, lr									\n\t"
-		"   ldr     r3, =hard_fault_handler_c               \n\t"
-		"   blx      r3                                      \n\t"
-		"pop {r0-r7}\n\t"
-		"mov r8, r0\n\t"
-		"mov r9, r1\n\t"
-		"mov r10, r2\n\t"
-		"mov r11, r3\n\t"
-		"   pop     {r4}                                    \n\t"
-		"   pop     {r3}                                    \n\t"
-		"   mov     lr, r3                                  \n\t"
-		// "   bx      lr                                      \n\t"
-	);
+	gpio_init(led_pin);
+	gpio_set_dir(led_pin, GPIO_OUT);
 
-	// dump_regs(tasks[current_task].sp);
-	// task1_func();
+	for (;;) {
+		gpio_put(led_pin, 1);
+		sleep_ms(500);
+		gpio_put(led_pin, 0);
+		sleep_ms(500);
+	}
+}
+
+void isr_hardfault(void)
+{
+	panic("%s\n", __func__);
 }
 
 void schedule(void)
 {
-	uint next = (current_task + 1) % MAX_TASKS;
+	/* A simple Round Robin scheduler */
+	// asm volatile("cpsid i");
+	current = &tasks[current_task];
+	current_task = (current_task + 1) % MAX_TASKS;
+	next = &tasks[current_task];
+	// asm volatile("cpsie i");
+
+	// static uint count = 0;
+	// pr_debug("%s-[%d], switch to {%d} | %08x, %08x\n", __func__, count++,
+	// 	 current_task, *current, *next);
 
 	/* This will trigger PendSV isr */
 	SCB->ICSR = SCB_ICSR_PENDSVSET_Msk;
@@ -192,88 +183,80 @@ void schedule(void)
 
 void isr_systick(void)
 {
-	static uint count = 0;
-	pr_debug("%s, %d\n", __func__, count++);
-
 	schedule();
 }
 
 void start_scheduler(void)
 {
+	/* the first task ready to run */
+	current = &tasks[0];
+
+	/* load the first task's sp position to the PSP */
 	__set_PSP((u32)tasks[0].sp);
 	__set_CONTROL(0x02);
 
-	printf("%s, PSP : 0x%08x\n", __func__, __get_PSP());
-	SCB->ICSR = SCB_ICSR_PENDSVSET_Msk;
+	/* trigger SVC and start the first task, see `isr_scvall` */
+	asm volatile("svc 0");
 
 	for (;;)
 		__WFI();
 }
 
-void task_init(struct task_struct *task, void (*entry)(void), u32 *sp_top)
+void task_init(struct task_struct *task, void (*entry)(void *data), u32 *sp_top,
+	       void *data)
 {
 	u32 *sp = sp_top;
 
-	// printf("%s, %p\n", __func__, sp_top);
-
 	/* r0, r1, r2, r3, r12, lr, pc, xpsr */
-	*(--sp) = 0x01000000;
+	*(--sp) = 0x01000000; /* Thumb mode */
 	*(--sp) = (u32)entry;
 	*(--sp) = 0xFFFFFFFD; /* Return to thread mode and use PSP */
-	*(--sp) = 0; // r12
-	*(--sp) = 0; // r3
-	*(--sp) = 0; // r2
-	*(--sp) = 0; // r1
-	*(--sp) = 0; // r0
+	*(--sp) = 0x12; // r12
+	*(--sp) = 0x03; // r3
+	*(--sp) = 0x02; // r2
+	*(--sp) = 0x01; // r1
+	*(--sp) = (u32)data; // r0
 
 	/* r4 ~ r11 */
-	for (int i = 0; i < 8; i++)
-		*(--sp) = 0;
+	for (int i = 0; i < 8; i++) {
+		// printf("%s, %p\n", __func__, sp);
+		*(--sp) = 4 + i;
+	}
 
-	// printf("%s, %p\n", __func__, sp);
-	task->sp = sp_top;
+	// dump_regs(sp);
+	task->sp = sp; /* save the current pos of it's stack */
+	printf("%s, %08X\n", __func__, task->sp);
 }
 
+static u32 task1_stack[STACK_SIZE] __attribute__((aligned(4)));
+static u32 task2_stack[STACK_SIZE] __attribute__((aligned(4)));
+static u32 task3_stack[STACK_SIZE] __attribute__((aligned(4)));
 int main()
 {
 	stdio_uart_init_full(uart0, 115200, 16, 17);
 	// stdio_usb_init();
 
-	pr_debug("\n\n\nsoocip starting...\n");
+	pr_debug("\n\n\nsoocip starting...\n\n");
 
-	const uint led_pin = PICO_DEFAULT_LED_PIN;
+	task_init(&tasks[0], task1_func, task1_stack + STACK_SIZE, "task-1");
+	task_init(&tasks[1], task2_func, task2_stack + STACK_SIZE, "task-2");
+	task_init(&tasks[2], task3_func, task3_stack + STACK_SIZE, "task-3");
 
-	gpio_init(led_pin);
-	gpio_set_dir(led_pin, GPIO_OUT);
+	printf("%p\n", task1_stack + STACK_SIZE);
+	// uint32_t *sp = tasks[0].sp + 8;
 
-	// struct task_struct dummy_task;
+	// printf("PSP = %p\n", sp);
+	// printf("r0  = %08X\n", sp[0]);
+	// printf("r1  = %08X\n", sp[1]);
+	// printf("r2  = %08X\n", sp[2]);
+	// printf("r3  = %08X\n", sp[3]);
+	// printf("r12 = %08X\n", sp[4]);
+	// printf("lr  = %08X\n", sp[5]);
+	// printf("pc  = %08X\n", sp[6]);
+	// printf("xpsr= %08X\n", sp[7]);
 
-	// dummy_task.sp = dummy_stack;
-	// __set_PSP((uint32_t)dummy_task.sp);
-
-	// printf("%p, %X", dummy_stack, __get_PSP());
-	static u32 task1_stack[STACK_SIZE];
-	static u32 task2_stack[STACK_SIZE];
-	printf("0x%p ~ 0x%p\n", task1_stack, task1_stack + STACK_SIZE);
-	// printf("0x%p\n", task2_stack);
-
-	task_init(&tasks[0], task1_func, task1_stack + STACK_SIZE);
-	// task_init(&tasks[1], task2_func, task2_stack + STACK_SIZE);
-
-	// printf("0x%p\n", tasks[0].sp + 16);
-	printf("0x%p\n", task1_func);
-	// printf("0x%p\n", task2_func);
-
-	// systick_init(10);
+	systick_init(10);
 
 	start_scheduler();
-
-	// while (true) {
-	// 	gpio_put(led_pin, 1);
-	// 	sleep_ms(50);
-	// 	gpio_put(led_pin, 0);
-	// 	sleep_ms(50);
-	// }
-
 	return 0;
 }
